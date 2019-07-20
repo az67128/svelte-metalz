827382981463b2cad32f7539a533df1d70f0bc51
Alexander Zinchenko <az67128@gmail.com>
2019-07-20 21:30:29 +0300

    mvp

--- src/App.svelte
+++ src/App.svelte
@@ -18,6 +18,7 @@
     height: 100%;
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
       Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    background: #f5f5f5;
   }
 </style>
 
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,11 +1,14 @@
 <script>
   import { afterUpdate } from "svelte";
+  import pannable from "../utils/pannable";
+  import { spring } from "svelte/motion";
   import Album from "./Album/index.svelte";
   import {
     sortedAlbums,
     isLoading,
     filterYandex,
-    filterGoogle
+    filterGoogle,
+    currentMonth
   } from "../stores/albums";
   import Loader from "./Loader.svelte";
 
@@ -13,24 +16,63 @@
   const scrollToTop = () => {
     if (container) container.scrollTop = 0;
   };
+
   filterYandex.subscribe(scrollToTop);
   filterGoogle.subscribe(scrollToTop);
+
+  // ------------------Pannable
+  let startX = 0;
+  const coords = spring(0, {
+    stiffness: 0.2,
+    damping: 0.4
+  });
+
+  function handlePanStart() {
+    startX = 0;
+    coords.stiffness = coords.damping = 1;
+  }
+
+  function handlePanMove(event) {
+    coords.update($coords => $coords + event.detail.dx);
+  }
+
+  function handlePanEnd(event) {
+    const delta = startX - $coords;
+
+    if (Math.abs(delta) < window.innerWidth / 3) {
+      coords.stiffness = 0.2;
+      coords.damping = 0.4;
+      coords.set(0);
+      return;
+    } else {
+      coords.stiffness = 1;
+      coords.damping = 1;
+      coords.set(0);
+    }
+    coords.set(0);
+    if (delta > 0) {
+      currentMonth.nextMonth();
+    } else {
+      currentMonth.prevMonth();
+    }
+  }
+  // ------------------
   let trigger;
   let limit = 11;
-  const options = {
-    root: document.querySelector(".main"),
-    rootMargin: "10px",
-    threshold: 1.0
-  };
-  const callback = function(entries, observer) {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        limit = +entry.target.getAttribute("index") + 10;
-      }
-    });
-  };
-  const observer = new IntersectionObserver(callback, options);
-
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          limit = +entry.target.getAttribute("index") + 10;
+        }
+      });
+    },
+    {
+      root: document.querySelector(".main"),
+      rootMargin: "10px",
+      threshold: 0.1
+    }
+  );
   afterUpdate(() => {
     if (trigger) observer.observe(trigger);
   });
@@ -43,24 +85,37 @@
     background: #f5f5f5;
   }
   @media (min-width: 600px) {
-    .main {
+    .wrapper {
       display: flex;
       flex-wrap: wrap;
       justify-content: space-between;
     }
   }
+  .wrapper {
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+  }
 </style>
 
 <main class="main" bind:this={container}>
-  {#if $isLoading}
-    <Loader />
-  {:else}
-    {#each $sortedAlbums.slice(0, limit) as album, index (album.album_id)}
-      <Album {album} />
-      {#if (index + 1) % 5 === 0 || index + 1 === limit}
-        <span bind:this={trigger} {index} />
-      {/if}
-    {/each}
-  {/if}
+  <div
+    class="wrapper"
+    use:pannable
+    on:panstart={handlePanStart}
+    on:panmove={handlePanMove}
+    on:panend={handlePanEnd}
+    style="transform: translate({$coords}px,0px)">
+    {#if $isLoading}
+      <Loader />
+    {:else}
+      {#each $sortedAlbums.slice(0, limit) as album, index (album.album_id)}
+        <Album {album} />
+        {#if (index + 1) % 5 === 0 || index + 1 === limit}
+          <span bind:this={trigger} {index} />
+        {/if}
+      {/each}
+    {/if}
+  </div>
 
 </main>
